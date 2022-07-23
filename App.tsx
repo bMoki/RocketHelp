b988605cdcf88595a35409e94da7406c2199489a
bMoki <breno26sonda@gmail.com>
2022-07-23 11:22:29 -0300

    Aula 2

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { NativeBaseProvider, StatusBar } from  'native-base';
 import { THEME } from './src/styles/theme';
-import { SignIn } from './src/screens/SignIn';
 import { Loading } from './src/components/Loading';
+import { Routes } from './src/routes';
 
 
 export default function App() {
@@ -15,7 +15,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <SignIn/> : <Loading/>}
+      { fontsLoaded ? <Routes/> : <Loading/>}
     </NativeBaseProvider>
     
   );
